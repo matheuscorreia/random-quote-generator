16f4e366d364c2c028b6bef4a448f325f0a7f1a4
matheuscorreia <matheuscorreia2005@gmail.com>
2017-06-07 20:33:24 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,69 @@
+<template>
+  <div id="app" v-bind:style="{'background-color':`rgba(${color.r}, ${color.g}, ${color.b}, ${color.a})`}">
+    <div class="header">
+      <span class="header__text">random</span>
+      <img src="src/assets/icons/feather.svg" class="header__icon" />
+      <span class="header__text">quote</span>
+    </div>
+    <quote v-on:changeColor="randomColor"></quote>
+  </div>
+</template>
+
+<script>
+import Quote from './components/Quote.vue';
+
+function randomBetween(min, max){
+  return Math.ceil(Math.random() * (max - min) + min);
+}
+
+export default {
+  name: 'app',
+  components: {
+    'quote': Quote
+  },
+  data: function () {
+    return {
+      color: {
+        r: 0,
+        g: 0,
+        b: 0,
+        a: 0.5,
+      }
+    }
+  },
+  methods: {
+    randomColor(){
+      this.color.r = randomBetween(0,255);
+      this.color.g = randomBetween(0,255);
+      this.color.b = randomBetween(0,255);
+      console.log(this.color);
+    }
+  }
+}
+</script>
+
+<style>
+#app{
+  height: 100%;
+  transition: 1s all ease-in-out;
+}
+html, body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+}
+html {
+  background: url('assets/img/bg.png');
+  background-repeat: repeat;
+  font-family: 'Raleway';
+}
+.header__text{
+  margin: 0 auto;
+  font-size: 3em;
+  font-weight: 300;
+  padding: 20px 0px 0px 20px;
+}
+.header__icon{
+  margin: 0px 0px 0px 20px;
+}
+</style>
---
+++ src/components/Quote.vue
@@ -0,0 +1,101 @@
+<template lang="html">
+  <div class="quote">
+    <div class="quote__showcase" v-bind:class="{ loading: !isLoading }">
+      <div class="quote__text" >
+        "{{ quoteText }}"
+      </div>
+      <div class="quote__author">
+        <p> - {{ quoteAuthor }}</p>
+      </div>
+    </div>
+    <div class="quote__buttons">
+      <button class="button" v-on:click="getQuote">
+        <img class="quote__icon" src="src/assets/icons/rotate-cw.svg" />
+      </button>
+      <a class="button" v-bind:href="shareURI" target="_blank">
+        <img class="quote__icon" src="src/assets/icons/twitter.svg" />
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+function decodeEntities(str){
+  return str.replace(/&#(\d+);/g, function(match, dec) {
+    return String.fromCharCode(dec);
+  });
+}
+
+export default {
+  data: function () {
+    return {
+      quoteText: "",
+      quoteAuthor: "",
+      quoteAuthorWiki: "",
+      isLoading: true,
+      shareURI: ""
+    }
+  },
+  methods: {
+    getQuote(){
+      this.isLoading = false;
+      let reqConfig = {
+        method: 'get',
+        url: 'http://www.quotzzy.co/api/quote/',
+        responseType: 'json'
+      }
+
+      axios(reqConfig).then(res => {
+        let tweet = `"${res.data.text}" - ${res.data.author.name}`
+        this.shareURI = encodeURI(`https://twitter.com/intent/tweet?text=${tweet}`)
+        this.quoteText = res.data.text
+        this.quoteAuthor = res.data.author.name
+        this.quoteAuthorWiki = res.data.author.wiki
+        this.isLoading = true;
+        this.$emit('changeColor')
+      })
+    }
+  },
+  mounted: function () {
+    this.getQuote()
+  }
+}
+</script>
+
+<style lang="css" scoped>
+div{
+  transition: 0.3s all ease-in-out;
+}
+.quote{
+  padding: 100px 50px 0 50px;
+  width: auto;
+  font-size: 2em;
+  /*text-align: justify;*/
+}
+
+.button {
+  background-color: transparent;
+  cursor: pointer;
+  border: none;
+}
+
+.quote__icon{
+  position: relative;
+  transform: rotate(0deg);
+  transition: .5s ease-in-out;
+}
+
+.quote__icon:hover{
+  transform: rotate(360deg);
+}
+
+.quote__author{
+  font-size: 0.5em;
+}
+
+.quote__showcase.loading{
+  filter: blur(10px);
+}
+</style>
